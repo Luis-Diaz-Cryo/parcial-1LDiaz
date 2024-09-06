1931600ad05691bd0b2ffba79b073ec1b300d754
cylde <cyldecryo@gmail.com>
2024-09-06 18:05:16 -0500

    implementacion de circuit breaker

--- Patrones-parcial/src/main/java/unisababa/edu/co/Patrones_parcial/service/MessageService.java
+++ Patrones-parcial/src/main/java/unisababa/edu/co/Patrones_parcial/service/MessageService.java
@@ -1,5 +1,6 @@
 package unisababa.edu.co.Patrones_parcial.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,18 +18,36 @@ public class MessageService {
     }
 
 
+    @CircuitBreaker(name = "messageService", fallbackMethod = "fallbackGuardarMessage")
     public Messages guardarMessage(Messages messages) {
 
         return repository.save(messages);
     }
-
+    @CircuitBreaker(name = "messageService", fallbackMethod = "fallbackConsultarMessages")
     public List<Messages> consultarMessages() {
         return repository.findAll();
     }
 
+    @CircuitBreaker(name = "messageService", fallbackMethod = "fallbackClearAllMessages")
     public void clearAllMessages() {
         repository.deleteAll();
     }
+
+    public Messages fallbackGuardarMessage(Messages messages, Throwable t) {
+        System.err.println("Error occurred while saving message: " + t.getMessage());
+        Messages fallbackMessage = new Messages();
+        fallbackMessage.setMessage("Default message content due to fallback");
+
+        return fallbackMessage;
+    }
+    public List<Messages> fallbackConsultarMessages(Throwable t) {
+        System.err.println("Error occurred while fetching messages: " + t.getMessage());
+        return List.of();
+    }
+    public void fallbackClearAllMessages(Throwable t) {
+        System.err.println("Error occurred while clearing messages: " + t.getMessage());
+    }
+
 
 
 
